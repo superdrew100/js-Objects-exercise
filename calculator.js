2f372c05e2ef0e638d2fdd22e5bfb71ee33cc310
Drew Thomasson <superdrew100@Gmail.com>
2020-04-03 08:46:00 -0400

    added repository

---
+++ calculator.js
@@ -0,0 +1,19 @@
+let calculator = {
+    Addition: function(a, b) {
+        return (this.Addition.a + this.Addition.b)
+    }
+    Subtraction: function(c, d) {
+        return (this.Subtraction.c - this.Subtraction.d)
+    }
+    Multiplication: function(e, f) {
+        return (this.Multiplication.e + this.Multiplication.f)
+    }
+    Division: function(g, h) {
+        return (this.Division.g/this.Division.h)
+    }
+    Exponents: function(k, l,) {
+        for(let i = 0; i > l ;i++) {
+        this.Exponents.k * this.Exponents.k
+    }
+    return k
+}
